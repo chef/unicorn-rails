28921e6f2f7a1e6dc406b0f1e6b4a51b96542b4e
talktovikas <vikas.yadav@progress.com>
2025-07-29 15:00:50 +0530

    changing the rack
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- lib/unicorn_rails.rb
+++ lib/unicorn_rails.rb
@@ -1,73 +1,48 @@
 require "unicorn"
 require "unicorn/launcher"
+require "rackup"             # bring in Rackup::Handler
 
 module UnicornRails
   require "unicorn_rails/version"
 end
 
-# Load rackup in case we’re on Rack 3+
-begin
-  require "rackup"
-rescue LoadError
-  # rackup not available, assume Rack < 3
-end
-
-# Define our handler class exactly as before
-module Rack
+# Define the Unicorn handler under Rackup::Handler
+module Rackup
   module Handler
     class Unicorn
       class << self
+        # Called by rackup when you do `-s unicorn`
         def run(app, options = {})
-          unicorn_options = {
-            listeners: ["#{options[:Host]}:#{options[:Port]}"]
-          }
+          unicorn_opts = { listeners: ["#{options[:Host]}:#{options[:Port]}"] }
 
-          env = environment
-          cfg = if ::File.exist?("config/unicorn/#{env}.rb")
-                  "config/unicorn/#{env}.rb"
-                elsif ::File.exist?("config/unicorn.rb")
-                  "config/unicorn.rb"
-                end
+          env = ENV["RACK_ENV"] || ENV["RAILS_ENV"]
+          cfg_path = if File.exist?("config/unicorn/#{env}.rb")
+                       "config/unicorn/#{env}.rb"
+                     elsif File.exist?("config/unicorn.rb")
+                       "config/unicorn.rb"
+                     end
 
-          if cfg
-            unicorn_options[:config_file] = cfg
-            # if that config already sets `listen`, don’t pass our default listeners
-            unicorn_options.delete(:listeners) if ::File.read(cfg) =~ /^\s*listen\s/
+          if cfg_path
+            unicorn_opts[:config_file] = cfg_path
+            # if your config already sets `listen`, drop our default listeners
+            unicorn_opts.delete(:listeners) if File.read(cfg_path) =~ /^\s*listen\s/
           else
-            unicorn_options[:timeout]          = 31 * 24 * 60 * 60
-            unicorn_options[:worker_processes] = (ENV["UNICORN_WORKERS"] || "1").to_i
+            unicorn_opts[:timeout]          = 31 * 24 * 60 * 60
+            unicorn_opts[:worker_processes] = (ENV["UNICORN_WORKERS"] || "1").to_i
           end
 
-          ::Unicorn::Launcher.daemonize!(unicorn_options) if options[:daemonize]
-          ::Unicorn::HttpServer.new(app, unicorn_options).start.join
-        end
-
-        def environment
-          ENV["RACK_ENV"] || ENV["RAILS_ENV"]
+          ::Unicorn::Launcher.daemonize!(unicorn_opts) if options[:daemonize]
+          ::Unicorn::HttpServer.new(app, unicorn_opts).start.join
         end
       end
     end
+
+    # Register the handler so `rackup -s unicorn` works
+    register "unicorn", Unicorn
   end
 end
 
-# Register under whichever handler module is available
-handler_mod =
-  if defined?(Rack::Handler) && Rack::Handler.respond_to?(:register)
-    Rack::Handler
-  elsif defined?(Rackup::Handler) && Rackup::Handler.respond_to?(:register)
-    Rackup::Handler
-  end
-
-if handler_mod
-  target = handler_mod == Rack::Handler ? "Rack::Handler::Unicorn" : "Rackup::Handler::Unicorn"
-  handler_mod.register "unicorn", target
-end
-
-# Provide a default hook for both Rack and rackup
+# Helper so Rails/Rake tasks can still resolve the handler if they call .default
 def self.default(options = {})
-  if defined?(Rackup::Handler) && Rackup::Handler.respond_to?(:get)
-    Rackup::Handler.get('unicorn')
-  else
-    Rack::Handler::Unicorn
-  end
+  Rackup::Handler.get("unicorn")
 end
